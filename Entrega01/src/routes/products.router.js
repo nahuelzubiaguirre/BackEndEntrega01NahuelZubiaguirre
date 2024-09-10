6c08a64dc21e8326f02fc0b8674444fc2e40fc4f
Nahuel Zubiaguirre <nahuelignacio_Z@hotmail.com>
2024-09-09 23:35:26 -0300

    first commit

---
+++ Entrega01/src/routes/products.router.js
@@ -0,0 +1,54 @@
+import {Router} from 'express'
+
+const router = Router ()
+
+let products = [] 
+
+router.get('/', (req, res) => {
+    res.json(products)
+})
+
+router.post ('/', (req, res) => {
+    const newProduct = req.body
+    users.push(newProduct)
+    res.status(201).json(newProduct)
+} )
+
+router.get ('/', (req, res) => {
+
+})
+
+router.post ('/', (req, res) => {
+    const {id, tittle, description, code, price, status, stock, category, thumbnails} = req.body
+    if(!id || !tittle || !description || !code || !price || !status || !stock || !category || !thumbnails){
+        return response.status(400).json({error: 'Daros invalidos'})
+
+    }    
+
+    const nuevoProducto = {
+        tittle,
+        description,
+        code,
+        price,
+        status,
+        stock,
+        category,
+        thumbnails
+    }
+
+    products.push(nuevoProducto)
+    response.status(201).json(nuevoProducto)
+})
+
+router.put ('/', (req, res) => {
+    
+})
+
+router.delete ('/', (req, res) => {
+    
+})
+
+
+
+
+export default router;
